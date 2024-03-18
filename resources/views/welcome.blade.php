13dd6ab7afd2b12ed9200006783384c78f560bff
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-03-18 11:24:34 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -129,5 +129,20 @@
                 </div>
             </div>
         </div>
+        <script>
+            if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+} else {
+  console.log("Geolocation is not supported by this browser.");
+}
+
+function successFunction(position) {
+  console.log(position);
+}
+
+function errorFunction() {
+  console.log("Unable to retrieve your location.");
+}
+        </script>
     </body>
 </html>
